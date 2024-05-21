b712953f6b04d7f33f68b408665ad8f0660c23db
nimishgo <nimish.2711@gmail.com>
2024-05-21 22:59:39 +0530

    fix : color, router

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customBlue: {
+          500: "#2aa7ff",
+        },
+      },
+    },
   },
   plugins: [],
 };
