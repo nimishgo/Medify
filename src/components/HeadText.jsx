d54cb685618cf46d182afc4469c16b7c8f375af1
nimishgo <nimish.2711@gmail.com>
2024-05-21 23:00:26 +0530

    add : header text

---
+++ src/components/HeadText.jsx
@@ -0,0 +1,10 @@
+import { Box } from "@radix-ui/themes";
+
+export default function HeadText() {
+  return (
+    <Box className="p-4 text-sm bg-customBlue-500 text-white text-center font-medium sm:text-lg sm:p-2">
+      The health and well-being of our patients and their health care team will
+      always be our priority, so we follow the best practices for cleanliness.
+    </Box>
+  );
+}
