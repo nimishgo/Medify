f13119835c622697a1cc5deb05bb5540d34670e4
nimishgo <nimish.2711@gmail.com>
2024-05-25 19:41:53 +0530

    add : img body

---
+++ src/components/Body.jsx
@@ -0,0 +1,17 @@
+import AdsCarousel from "./AdsCarousel";
+import DocGrid from "./DocGrid";
+import DrCarousel from "./DrCarousel";
+import Footer3 from "./Footer3";
+import SearchContainer from "./SearchContainer";
+
+export default function Body() {
+  return (
+    <>
+      <SearchContainer />
+      <AdsCarousel />
+      <DocGrid />
+      <DrCarousel />
+      <Footer3 />
+    </>
+  );
+}
